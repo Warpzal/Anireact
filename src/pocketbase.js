a7bbc652b02c852ee7e0ff9b2b19da8be39d4d03
Zalonics <anthony.dev.9898@gmail.com>
2022-10-01 08:24:05 -0700

    Proper Backend

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase'
 
-const domain = 'http://127.0.0.1:8090'
+const domain = 'https://anireact.fly.dev/'
 const client = new PocketBase(domain)
 
 export { client, domain }
